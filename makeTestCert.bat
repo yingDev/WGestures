bfd4d5594d5c6ba360558023012cbda08d72766f
yingDev <ying@yingDev.com>
2014-11-08 02:52:49 +0800

    修正测试证书脚本

--- makeTestCert.bat
+++ makeTestCert.bat
@@ -1,6 +1,6 @@
 makecert -r -pe -n "CN=WGestures Test CA" -ss CA -sr CurrentUser -a sha256 -cy authority -sky signature -sv WGesturesTestCA.pvk WGesturesTestCA.cer
 
-makecert -pe -n "CN=YingDev.com SPC" -a sha256 -cy end -sky signature -ic WGesturesTestCA.cer -iv WGesturesTestCA.pvk  -sv WGesturesTestSPC.pvk WGesturesTestSPC.cer
+makecert -pe -n "CN=WGestures Test SPC" -a sha256 -cy end -sky signature -ic WGesturesTestCA.cer -iv WGesturesTestCA.pvk  -sv WGesturesTestSPC.pvk WGesturesTestSPC.cer
 
 pvk2pfx -pvk WGesturesTestSPC.pvk -spc WGesturesTestSPC.cer -pfx WGesturesTestSPC.pfx
 
