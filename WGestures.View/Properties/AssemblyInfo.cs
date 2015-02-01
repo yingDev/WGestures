00ab031a67757d550e07c9e119b56cd0b83b77dd
yingDev <ying@yingDev.com>
2015-02-01 11:21:15 +0800

    Remove log4net

--- WGestures.View/Properties/AssemblyInfo.cs
+++ WGestures.View/Properties/AssemblyInfo.cs
@@ -34,6 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.4.0.0")]
 [assembly: AssemblyFileVersion("1.4.0.0")]
-#if DEBUG
-[assembly: log4net.Config.XmlConfigurator]
-#endif
