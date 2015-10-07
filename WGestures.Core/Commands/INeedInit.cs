dc67b82e2445d43493b4d8c79d8da7a92e072fd1
Ying Yuandong <897326609@qq.com>
2015-10-08 00:29:20 +0800

    back to .net 3.5

--- WGestures.Core/Commands/INeedInit.cs
+++ WGestures.Core/Commands/INeedInit.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WGestures.Core.Commands
+﻿namespace WGestures.Core.Commands
 {
     interface INeedInit
     {
