af3b25d0dbf8d519f79d60f91b64d169a4762962
YingDev <897326609@qq.com>
2015-10-19 00:48:05 +0800

    bigger stepSize

--- WGestures.Core/Impl/Windows/Win32MousePathTracker2.cs
+++ WGestures.Core/Impl/Windows/Win32MousePathTracker2.cs
@@ -201,7 +201,7 @@ namespace WGestures.Core.Impl.Windows
             
 
             EffectiveMove = (int)(10 * dpiFactor) * 2;//todo: 增加灵敏度调整
-            StepSize = (int) (2*dpiFactor);// EffectiveMove/8;
+            StepSize = EffectiveMove;// (int) (EffectiveMove * 0.8 * dpiFactor);// EffectiveMove/8;
             StayTimeout = false;
             PerformNormalWhenTimeout = false;
 
