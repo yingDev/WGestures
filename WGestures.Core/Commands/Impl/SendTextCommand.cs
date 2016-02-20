dbb26915fd505a21bafd5beabd6f53960ab9e82c
Ying Yuandong <897326609@qq.com>
2016-02-20 17:40:45 +0800

    support {sleep x}

--- WGestures.Core/Commands/Impl/SendTextCommand.cs
+++ WGestures.Core/Commands/Impl/SendTextCommand.cs
@@ -9,6 +9,7 @@ using WGestures.Common.Annotation;
 using WGestures.Common.OsSpecific.Windows;
 using Win32;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace WGestures.Core.Commands.Impl
 {
@@ -21,7 +22,25 @@ namespace WGestures.Core.Commands.Impl
         {
             if (!string.IsNullOrEmpty(Text))
             {
-                SendKeys.SendWait(Text);
+                var lines = new Regex(@"({(?i)sleep(?-i) *[0-9]*})").Split(Text);
+                var timeExtract = new Regex("{(?i)sleep(?-i) *([0-9]*)}");
+
+                foreach(var l in lines)
+                {
+                    var match = timeExtract.Match(l);
+                    if (match.Success)
+                    {
+                        int delayMs;
+                        if(int.TryParse(match.Groups[1].Value, out delayMs))
+                        {
+                            Thread.Sleep(delayMs);
+                            continue;
+                        }
+                    }
+
+                    SendKeys.SendWait(l);
+                }
+                
             }
 
             GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
